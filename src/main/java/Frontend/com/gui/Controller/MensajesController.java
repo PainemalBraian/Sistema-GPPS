7a3494d5a30baf15ae17ea08bb78b23c04a2cca7
Agustin <114381140+PueblaTL@users.noreply.github.com>
2025-05-19 22:42:51 -0300

    pantalla de mensajes
    vista de la pantalla para mensajes entre estudiantes, tutores y docentes. Falta implementar comportamiento y crear los dto de mensaje y conversaciones.

---
+++ src/main/java/Frontend/com/gui/Controller/MensajesController.java
@@ -0,0 +1,14 @@
+package Frontend.com.gui.Controller;
+
+import javafx.event.ActionEvent;
+
+public class MensajesController {
+    public void handleNuevaConversacion(ActionEvent actionEvent) {
+    }
+
+    public void handleEnviarMensaje(ActionEvent actionEvent) {
+    }
+
+    public void handleVolverHome(ActionEvent actionEvent) {
+    }
+}
